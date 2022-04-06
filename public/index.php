9639e9345ca497aadb40b09e9db6ac5f2a54e1db
daniel <danielmessi485@gmail.com>
2022-04-05 19:57:55 -0600

    backend
    rutas creadas

--- public/index.php
+++ public/index.php
@@ -1,21 +1,339 @@
 <?php
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Selective\BasePath\BasePathMiddleware;
 use Slim\Factory\AppFactory;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/Models/db.php';
+
+
+
+$bd = new BD();
+$bd = $bd->conexionBD();
 
 $app = AppFactory::create();
+$app->addBodyParsingMiddleware();
+$app->setBasePath('/proyecto-grupal-backend/public');
 
-$app->addRoutingMiddleware();
-$app->add(new BasePathMiddleware($app));
-$app->addErrorMiddleware(true, true, true);
-
-$app->get('/', function (Request $request, Response $response) {
-   $response->getBody()->write('Hello World!');
-   return $response;
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
 });
+
+
+
+/**=======================================================================
+ * =======================================================================
+ * SECCIÓN DE GASTOS FIJOS
+ * CONSULTAR, AÑADIR, MODIFICAR, DAR DE BAJA
+ * =======================================================================
+ */
+//consultar gastos fijos
+
+$app->get('/api/costos_fijos/consultar', function(Request $request, Response $response){
+   $consulta = 'SELECT * FROM gastos_fijos';
+   try{
+       $db = new BD();
+       $db = $db->conexionBD();
+       $ejecutar = $db->query($consulta);
+       $gastos = $ejecutar->fetchAll(PDO::FETCH_OBJ);
+       $db = null;
+       $response->getBody()->write(json_encode($gastos));
+       return $response;// $gastos;
+   } catch(PDOException $e){
+       echo '{"error": {"text":  '.$e->getMessage().'}';
+   }
+});
+
+//Añadir gastos fijos
+$app->post('/api/costos_fijos/add', function(Request $request, Response $response, array $args){
+   $data = $request->getParsedBody();
+   
+   $tipo_gasto = $data["tipo_gasto"];
+   $descripcion = $data["descripcion"];
+   $cantidad = $data["cantidad"];
+   $fecha = $data["fecha"];
+   $id_sucursal = $data["id_sucursal"];
+   $periodicidad = $data["periodicidad"];
+   $status = $data["status"];
+
+   $sql = "INSERT INTO gastos_fijos (tipo_gasto, descripcion, cantidad, fecha, id_sucursal, periodicidad, status) VALUES 
+           (:tipo_gasto, :descripcion, :cantidad, :fecha, :id_sucursal, :periodicidad, :status)";
+   try {
+       $db = new BD();
+       $db = $db->conexionBD();
+       $resultado = $db->prepare($sql);
+       $resultado->bindParam(':tipo_gasto', $tipo_gasto);
+       $resultado->bindParam(':descripcion', $descripcion);
+       $resultado->bindParam(':cantidad', $cantidad);
+       $resultado->bindParam(':fecha', $fecha);
+       $resultado->bindParam(':id_sucursal', $id_sucursal);
+       $resultado->bindParam(':periodicidad', $periodicidad);
+       $resultado->bindParam(':status', $status);
+
+       $resultado->execute();
+       //echo json_encode("Nuevo gasto fijo agregado.");
+       
+       $db = null;
+
+       $response->getBody()->write(json_encode($resultado));
+       return $response 
+           ->withHeader('content-type','aplication/json')
+           ->withStatus(200);
+           $resultado = null;
+   } catch (PDOException $e) {
+       //echo '{"error": {"text":  '.$e->getMessage().'}';
+       $error = array(
+           "message" => $e->getMessage()
+       );
+
+       $response->getBody()->write(json_encode($error));
+       return $response
+           ->withHeader('content-type','aplication/json')
+           ->withStatus(500);
+   }
+});
+
+//Modificar gasto fijo
+$app->put('/api/costos_fijos/update/{id}',function (Request $request, Response $response, array $args) {
+   $id = $request->getAttribute('id');
+   $data = $request->getParsedBody();
+   $tipo_gasto = $data["tipo_gasto"];
+   $descripcion = $data["descripcion"];
+   $cantidad = $data["cantidad"];
+   $fecha = $data["fecha"];
+   $id_sucursal = $data["id_sucursal"];
+   $periodicidad = $data["periodicidad"];
+   $status = $data["status"];
+  
+   $sql = "UPDATE gastos_fijos SET
+             tipo_gasto = :tipo_gasto,
+             descripcion = :descripcion,
+             cantidad = :cantidad,
+             fecha  = :fecha,
+             id_sucursal = :id_sucursal,
+             periodicidad = :periodicidad,
+             status = :status
+   WHERE id_gasto = '$id' ";
+  
+   try {
+     $db = new BD();
+     $db = $db->conexionBD();
+    
+     $resultado = $db->prepare($sql);
+     $resultado->bindParam(':tipo_gasto', $tipo_gasto);
+     $resultado->bindParam(':descripcion', $descripcion);
+     $resultado->bindParam(':cantidad', $cantidad);
+     $resultado->bindParam(':fecha', $fecha);
+     $resultado->bindParam(':id_sucursal', $id_sucursal);
+     $resultado->bindParam(':periodicidad', $periodicidad);
+     $resultado->bindParam(':status', $status);
+
+     $resultado->execute();
+  
+     $db = null;
+     
+     echo "Update successful! ";
+     $response->getBody()->write(json_encode($resultado));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(200);
+     $resultado = null;
+   } catch (PDOException $e) {
+     $error = array(
+       "message" => $e->getMessage()
+     );
+  
+     $response->getBody()->write(json_encode($error));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(500);
+   }
+});
+
+
+//Dar de baja
+$app->put('/api/costos_fijos/baja/{id}',function (Request $request, Response $response, array $args) {
+   $id = $request->getAttribute('id');
+   $data = $request->getParsedBody();
+   $status = $data["status"];
+  
+   $sql = "UPDATE gastos_fijos SET
+             status = :status
+   WHERE id_gasto = '$id' ";
+  
+   try {
+     $db = new BD();
+     $db = $db->conexionBD();
+    
+     $resultado = $db->prepare($sql);
+     $resultado->bindParam(':status', $status);
+
+     $resultado->execute();
+  
+     $db = null;
+     
+     echo "Update successful! ";
+     $response->getBody()->write(json_encode($resultado));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(200);
+     $resultado = null;
+   } catch (PDOException $e) {
+     $error = array(
+       "message" => $e->getMessage()
+     );
+  
+     $response->getBody()->write(json_encode($error));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(500);
+   }
+});
+
+/**=======================================================================
+ * =======================================================================
+ * SECCIÓN DE TIPO DE GASTOS
+ * CONSULTAR, AÑADIR, MODIFICAR, DAR DE BAJA
+ * 
+ * =======================================================================
+ */
+//consultar TIPO DE GASTOS
+
+$app->get('/api/tipo_gastos/consultar', function(Request $request, Response $response){
+   $consulta = 'SELECT * FROM tipo_gasto';
+   try{
+       $db = new BD();
+       $db = $db->conexionBD();
+       $ejecutar = $db->query($consulta);
+       $gastos = $ejecutar->fetchAll(PDO::FETCH_OBJ);
+       $db = null;
+       $response->getBody()->write(json_encode($gastos));
+       return $response;// $gastos;
+   } catch(PDOException $e){
+       echo '{"error": {"text":  '.$e->getMessage().'}';
+   }
+});
+
+//Añadir TIPO GASTO
+$app->post('/api/tipo_gastos/add', function(Request $request, Response $response, array $args){
+   $data = $request->getParsedBody();
+   $nombre = $data["nombre"];
+   $status = $data["status"];
+   
+   $sql = "INSERT INTO tipo_gasto ( nombre, status) VALUES 
+           (:nombre, :status)";
+   try {
+       $db = new BD();
+       $db = $db->conexionBD();
+       $resultado = $db->prepare($sql);
+       $resultado->bindParam(':nombre', $nombre);
+       $resultado->bindParam(':status', $status);
+
+       $resultado->execute();
+       $db = null;
+
+       $response->getBody()->write(json_encode($resultado));
+       return $response 
+           ->withHeader('content-type','aplication/json')
+           ->withStatus(200);
+           $resultado = null;
+   } catch (PDOException $e) {
+       //echo '{"error": {"text":  '.$e->getMessage().'}';
+       $error = array(
+           "message" => $e->getMessage()
+       );
+
+       $response->getBody()->write(json_encode($error));
+       return $response
+           ->withHeader('content-type','aplication/json')
+           ->withStatus(500);
+   }
+});
+
+//Modificar TIPO GASTO
+$app->put('/api/tipo_gastos/update/{id}',function (Request $request, Response $response, array $args) {
+   $id = $request->getAttribute('id');
+   $data = $request->getParsedBody();
+
+   $nombre = $data["nombre"];
+   $status = $data["status"];
+  
+   $sql = "UPDATE tipo_gasto SET
+             nombre = :nombre,
+             status = :status
+   WHERE id_tipo = '$id' ";
+  
+   try {
+     $db = new BD();
+     $db = $db->conexionBD();
+    
+     $resultado = $db->prepare($sql);
+
+     $resultado->bindParam(':nombre', $nombre);
+     $resultado->bindParam(':status', $status);
+
+     $resultado->execute();
+  
+     $db = null;
+     
+     echo "Update successful! ";
+     $response->getBody()->write(json_encode($resultado));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(200);
+     $resultado = null;
+   } catch (PDOException $e) {
+     $error = array(
+       "message" => $e->getMessage()
+     );
+  
+     $response->getBody()->write(json_encode($error));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(500);
+   }
+});
+
+//Dar de baja TIPO GASTO
+$app->put('/api/tipo_gastos/baja/{id}',function (Request $request, Response $response, array $args) {
+   $id = $request->getAttribute('id');
+   $data = $request->getParsedBody();
+   $status = $data["status"];
+  
+   $sql = "UPDATE tipo_gasto SET
+             status = :status
+   WHERE id_tipo = '$id' ";
+  
+   try {
+     $db = new BD();
+     $db = $db->conexionBD();
+    
+     $resultado = $db->prepare($sql);
+     $resultado->bindParam(':status', $status);
+
+     $resultado->execute();
+  
+     $db = null;
+     
+     echo "Update successful! ";
+     $response->getBody()->write(json_encode($resultado));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(200);
+     $resultado = null;
+   } catch (PDOException $e) {
+     $error = array(
+       "message" => $e->getMessage()
+     );
+  
+     $response->getBody()->write(json_encode($error));
+     return $response
+       ->withHeader('content-type', 'application/json')
+       ->withStatus(500);
+   }
+});
+
+
 
 $app->run();
