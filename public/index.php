6e462c67e33663c19f089e261eee46c0d180686b
daniel <danielmessi485@gmail.com>
2022-05-24 03:04:43 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Selective\BasePath\BasePathMiddleware;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/Models/db.php';
 
 
