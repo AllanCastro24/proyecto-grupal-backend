aff10f98983b69e08a59b762bf14f2a43c39ffb5
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-05-24 00:19:52 -0600

    modelo anterior

--- src/Models/db.php
+++ src/Models/db.php
@@ -1,16 +1,33 @@
 <?php
 
-class BD{
+namespace App\Models;
+
+use \PDO;
+
+class DB
+{
+    /* private $host = 'localhost';
+    private $user = 'root';
+    private $pass = '1234';
+    private $dbname = 'almacen'; 
+    
+    IP.- 187.216.118.68
+    DB.- pruebas
+    usr.- pruebas
+    pswd.- xtPduvLlPHo8IPVE
+    */
+
     private $host = '187.216.118.68';
     private $user = 'pruebas';
     private $pass = 'xtPduvLlPHo8IPVE';
-    private $nombreBD = 'pruebas';
+    private $dbname = 'pruebas';
 
-    public function conexionBD(){
-        $mysqlConn = "mysql:host=$this->host;dbname=$this->nombreBD";
-        $dbConn = new PDO($mysqlConn, $this->user, $this->pass);
-        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        return $dbConn;
+    public function connect()
+    {
+        $conn_str = "mysql:host=$this->host;dbname=$this->dbname";
+        $conn = new PDO($conn_str, $this->user, $this->pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $conn;
     }
 }
